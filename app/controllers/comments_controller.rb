c6c8ad27c76639b9589b9d2110df41328357f87a
Aleksandra Zurawska <aleksandra016@wp.pl>
2016-08-03 15:34:22 +0200

    vjxnvjxkuvh

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,6 +25,7 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
+    
 
     respond_to do |format|
       if @comment.save
